faeb226ea103192cbf6191191438c48c3d43212d
alessaba <alessandrosaba023@gmail.com>
2024-10-17 11:37:59 +0200

    Initial Commit

---
+++ HapticMassage/HapticMassageApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticMassageApp.swift
+//  HapticMassage
+//
+//  Created by Alessandro Saba on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HapticMassageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
