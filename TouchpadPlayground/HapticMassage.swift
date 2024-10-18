36da4b02cf8f64b2a1d913f1ee0e1c33cf825ad5
alessaba <alessandrosaba023@gmail.com>
2024-10-18 18:55:19 +0200

    Simplify Intensity

--- TouchpadPlayground/HapticMassage.swift
+++ TouchpadPlayground/HapticMassage.swift
@@ -9,39 +9,21 @@ import SwiftUI
 
 fileprivate let cid = CGSMainConnectionID()
 
-func pattern(for intensity: Double) -> Int {
-	// 8 valid pattern values are 0x1-0x6 and 0x0f-0x10
-	switch intensity {
-	case 0:
-		return 15
-	case 3:
-		return 6
-	default:
-		return Int(intensity)
-	}
-}
-
 struct HapticMassage: View {
 	@State var speed = 20.0
-	@State var intensity = 1.5
+	@State var intensity = 1
 	@State var hoveringMassage: Bool = false
-	@State var editingParameters: Bool = false
 	
 	var body: some View {
 		VStack{
 			HStack {
 				Slider(value: $speed, in: 5...100){
-					Text(editingParameters ? "\(Int(speed)) Hz": "Speed")
+					Text("Frequency: \(Int(speed)) Hz")
 				}.tint((speed > 75) ? .red : .green )
 				
-				Slider(value: $intensity, in: 0...3){
-					Text(editingParameters ? "\(pattern(for: intensity))": "Intensity")
-				}.tint((intensity > 2) ? .red : .green )
+				Stepper("Intensity: \(["Low ", "Med ", "High"][Int(intensity)])", value: $intensity, in: 0...2)
 			}
 			.padding()
-			.onHover{_ in
-				editingParameters.toggle()
-			}
 			
 			ZStack{
 				Rectangle().fill(.gray)
@@ -49,7 +31,8 @@ struct HapticMassage: View {
 						hoveringMassage.toggle()
 						Timer.scheduledTimer(withTimeInterval: TimeInterval(1/speed), repeats: true){ timer in
 							if hoveringMassage {
-								CGSActuateDeviceWithPattern(cid, 0, Int32(pattern(for: intensity)), 0);
+								// 8 valid pattern values are 0x1-0x6 and 0x0f-0x10 (1-6, 15, 16)
+								CGSActuateDeviceWithPattern(cid, 0, [0xf, 0x1, 0x2][intensity], 0)
 							} else {
 								timer.invalidate()
 							}
